868f08f176cab167823a69f92d09f1f6a7599aec
likux35 <starkus.lukas@inbox.lt>
2019-08-22 00:54:29 +0300

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -2,6 +2,7 @@
 
   // Change this to your GitHub username so you don't have to modify so many things.
   var fork = "likux35";
+  this.resp = this.https.get(this.apiURL ); //try this
 
   // Define our function responsible for extending the bot.
   function extend() {
