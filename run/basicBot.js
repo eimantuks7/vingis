d8a0a26af16c6fb100a10045deee8bacf71be56c
likux35 <starkus.lukas@inbox.lt>
2019-08-22 00:27:52 +0300

    Update basicBot.js

--- run/basicBot.js
+++ run/basicBot.js
@@ -1,9 +1,12 @@
 /**
 
- Copyright © 2014-2018 basicBot
+ Copyright © 2014-2019 basicBot
 
- Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
- This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
+ BasicBot v2
+
+ Lukasenka edition
+ 
+ Original Source : https://github.com/basicBot/source
 
  */
 
